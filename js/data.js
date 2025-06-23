ee03da7796a86264cb7d4f671a12e17779621840
YSC <dashuchiu@gmail.com>
2025-06-23 15:07:51 +0800

    v2

--- js/data.js
+++ js/data.js
@@ -1,23 +1,30 @@
 let data = [
   {
     id: 1,
-    title: "CHUNCHUN STUDIO",
-    desc: "甜點工作室的靜態網頁。",
-    tags: ["HTML", "CSS", "JavaScript"],
-    url: "./chunchunStudio/index.html",
+    title: "PETSLOVE",
+    desc: "寵物領養的靜態網頁。",
+    tags: ["HTML", "SCSS", "JavaScript"],
+    url: "https://petslove-q45l.onrender.com",
   },
   {
     id: 2,
+    title: "CHUNCHUN STUDIO",
+    desc: "甜點工作室的靜態網頁。",
+    tags: ["HTML", "SCSS", "JavaScript"],
+    url: "./chunchunStudio/index.html",
+  },
+  {
+    id: 3,
     title: "Get Coupons",
     desc: "找尋或發布分享優惠券的網站。",
     tags: ["HTML", "CSS", "JavaScript", "Bootstrap", "Vue"],
     url: "https://vue-coupons.firebaseapp.com/coupons",
   },
   {
-    id: 3,
+    id: 4,
     title: "Get Lucky",
     desc: "抽籤網頁。",
     tags: ["HTML", "CSS", "JavaScript"],
-    url: "./get_lucky/index.html",
+    url: "./petslove/index.html",
   },
 ];
